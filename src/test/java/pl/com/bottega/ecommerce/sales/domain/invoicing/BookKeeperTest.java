a05b505d5f33451ebbc7a677bf7e3f0bf4e5eefc
Adam Dąbrowski <190139@edu.p.lodz.pl>
2019-04-25 17:41:50 +0200

    isolation Add test that checks if only one invoiceFactory is used once

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -1,6 +1,7 @@
 package pl.com.bottega.ecommerce.sales.domain.invoicing;
 
 import org.junit.Test;
+import org.mockito.Mockito;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
@@ -96,5 +97,27 @@ public class BookKeeperTest {
 
         bookKeeper.issuance(invoiceRequest, taxPolicy);
         verify(taxPolicy, times(2)).calculateTax(any(), any());
+    }
+
+    @Test
+    public void issuanceShouldCreateOneInvoice() {
+        InvoiceFactory invoiceFactory = Mockito.spy(new InvoiceFactory());
+        BookKeeper bookKeeper = new BookKeeper(invoiceFactory);
+
+        ClientData clientData = mock(ClientData.class);
+        InvoiceRequest invoiceRequest = new InvoiceRequest(clientData);
+
+        ProductData productData = mock(ProductData.class);
+        Money money = new Money(0);
+        RequestItem requestItem = new RequestItem(productData, 1, money);
+        invoiceRequest.add(requestItem);
+        invoiceRequest.add(requestItem);
+
+        TaxPolicy taxPolicy = mock(TaxPolicy.class);
+        Tax tax = new Tax(new Money(0), "");
+        when(taxPolicy.calculateTax(any(), any())).thenReturn(tax);
+
+        bookKeeper.issuance(invoiceRequest, taxPolicy);
+        verify(invoiceFactory, times(1)).create(any());
     }
 }
