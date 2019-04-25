40d326309162ec267138287900666285754661c8
Adam Dąbrowski <190139@edu.p.lodz.pl>
2019-04-25 17:26:37 +0200

    isolation Implement 2nd sample test case

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -8,8 +8,7 @@ import pl.com.bottega.ecommerce.sharedkernel.Money;
 import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.*;
 import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class BookKeeperTest {
 
@@ -32,5 +31,27 @@ public class BookKeeperTest {
 
         Invoice invoice = bookKeeper.issuance(invoiceRequest, taxPolicy);
         assertThat(invoice.getItems(), hasSize(1));
+    }
+
+    @Test
+    public void issuanceShouldCalculateTaxForEveryItemInRequest() {
+        InvoiceFactory invoiceFactory = new InvoiceFactory();
+        BookKeeper bookKeeper = new BookKeeper(invoiceFactory);
+
+        ClientData clientData = mock(ClientData.class);
+        InvoiceRequest invoiceRequest = new InvoiceRequest(clientData);
+
+        ProductData productData = mock(ProductData.class);
+        Money money = new Money(0);
+        RequestItem requestItem = new RequestItem(productData, 1, money);
+        invoiceRequest.add(requestItem);
+        invoiceRequest.add(requestItem);
+
+        TaxPolicy taxPolicy = mock(TaxPolicy.class);
+        Tax tax = new Tax(new Money(0), "");
+        when(taxPolicy.calculateTax(any(), any())).thenReturn(tax);
+
+        bookKeeper.issuance(invoiceRequest, taxPolicy);
+        verify(taxPolicy, times(2)).calculateTax(any(), any());
     }
 }
