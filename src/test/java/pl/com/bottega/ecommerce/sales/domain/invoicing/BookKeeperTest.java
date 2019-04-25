dfdf4ad9506a83ea4cf057644dcb886f0a48a913
Adam Dąbrowski <190139@edu.p.lodz.pl>
2019-04-25 17:34:15 +0200

    isolation Add test that check for correct net amount

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -5,6 +5,7 @@ import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.*;
 import static org.mockito.Matchers.any;
@@ -31,6 +32,27 @@ public class BookKeeperTest {
 
         Invoice invoice = bookKeeper.issuance(invoiceRequest, taxPolicy);
         assertThat(invoice.getItems(), hasSize(1));
+    }
+
+    @Test
+    public void issuanceShouldAssignCorrectNet() {
+        InvoiceFactory invoiceFactory = new InvoiceFactory();
+        BookKeeper bookKeeper = new BookKeeper(invoiceFactory);
+
+        ClientData clientData = mock(ClientData.class);
+        InvoiceRequest invoiceRequest = new InvoiceRequest(clientData);
+
+        ProductData productData = mock(ProductData.class);
+        Money money = new Money(100);
+        RequestItem requestItem = new RequestItem(productData, 1, money);
+        invoiceRequest.add(requestItem);
+
+        TaxPolicy taxPolicy = mock(TaxPolicy.class);
+        Tax tax = new Tax(new Money(0), "");
+        when(taxPolicy.calculateTax(any(), any())).thenReturn(tax);
+
+        Invoice invoice = bookKeeper.issuance(invoiceRequest, taxPolicy);
+        assertThat(invoice.getNet(), is(money));
     }
 
     @Test
